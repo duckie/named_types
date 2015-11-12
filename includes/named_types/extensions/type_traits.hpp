1e8494ee1ae32504e2a024839b496e3afd73559b
Jean-Bernard Jansen <jean-bernard.jansen@winamax.fr>
2015-11-12 09:45:07 +0000

    Spare some static data by inheriting integral_constant

--- includes/named_types/extensions/type_traits.hpp
+++ includes/named_types/extensions/type_traits.hpp
@@ -11,153 +11,154 @@
 
 namespace named_types {
 
-template <class T> struct is_nullable {
-  static constexpr bool const value =
-      std::is_pointer<T>::value || std::is_assignable<T, std::nullptr_t>::value;
+template <class T>
+struct is_nullable
+    : std::integral_constant<bool,
+                             std::is_pointer<T>::value ||
+                                 std::is_assignable<T, std::nullptr_t>::value> {
 };
 
-template <class T> struct is_char {
-  using raw_type = typename std::remove_cv<T>::type;
-  static constexpr bool const value = std::is_same<char, raw_type>::value ||
-                                      std::is_same<wchar_t, raw_type>::value ||
-                                      std::is_same<char16_t, raw_type>::value ||
-                                      std::is_same<char32_t, raw_type>::value;
-};
+template <class T>
+struct is_char : std::integral_constant<
+                     bool,
+                     std::is_same<char, std::remove_cv_t<T>>::value ||
+                         std::is_same<wchar_t, std::remove_cv_t<T>>::value ||
+                         std::is_same<char16_t, std::remove_cv_t<T>>::value ||
+                         std::is_same<char32_t, std::remove_cv<T>>::value> {};
 
 // is_basic_string
 
-template <class T> struct is_std_basic_string {
-  static constexpr bool const value = false;
-};
+template <class T>
+struct is_std_basic_string : std::integral_constant<bool, false> {};
 
 template <class CharT, class Traits, class Allocator>
-struct is_std_basic_string<std::basic_string<CharT, Traits, Allocator>> {
-  static constexpr bool const value = true;
-};
+struct is_std_basic_string<std::basic_string<CharT, Traits, Allocator>>
+    : std::integral_constant<bool, true> {};
 
 // is_raw_string
 
-template <class T> struct is_raw_string {
-  static constexpr bool const value = false;
-};
+template <class T>
+struct is_raw_string : std::integral_constant<bool, false> {};
 
-template <class CharT, size_t Length> struct is_raw_string<CharT[Length]> {
-  static constexpr bool const value = is_char<CharT>::value;
-};
+template <class CharT, size_t Length>
+struct is_raw_string<CharT[Length]>
+    : std::integral_constant<bool, is_char<CharT>::value> {};
 
-template <class CharT> struct is_raw_string<CharT*> {
-  static constexpr bool const value = is_char<CharT>::value;
-};
+template <class CharT>
+struct is_raw_string<CharT*>
+    : std::integral_constant<bool, is_char<CharT>::value> {};
 
 // is_sequence_container
 
-template <class T> struct is_sequence_container {
-  static constexpr bool const value = false;
-};
+template <class T>
+struct is_sequence_container : std::integral_constant<bool, false> {};
 
 template <class T, class Allocator>
-struct is_sequence_container<std::vector<T, Allocator>> {
-  static constexpr bool const value = true;
-};
+struct is_sequence_container<std::vector<T, Allocator>>
+    : std::integral_constant<bool, true> {};
 
 template <class T, class Allocator>
-struct is_sequence_container<std::list<T, Allocator>> {
-  static constexpr bool const value = true;
-};
+struct is_sequence_container<std::list<T, Allocator>>
+    : std::integral_constant<bool, true> {};
 
 // is_associative_container
 
-template <class T> struct is_associative_container {
-  static constexpr bool const value = false;
-};
+template <class T>
+struct is_associative_container : std::integral_constant<bool, false> {};
 
 template <class T, class Allocator>
-struct is_associative_container<std::map<T, Allocator>> {
-  static constexpr bool const value = true;
-};
+struct is_associative_container<std::map<T, Allocator>>
+    : std::integral_constant<bool, true> {};
 
 template <class T, class Allocator>
-struct is_associative_container<std::unordered_map<T, Allocator>> {
-  static constexpr bool const value = true;
-};
+struct is_associative_container<std::unordered_map<T, Allocator>>
+    : std::integral_constant<bool, true> {};
 
 // is_named_tuple
 
-template <class T> struct is_named_tuple {
-  static constexpr bool const value = false;
-};
+template <class T>
+struct is_named_tuple : std::integral_constant<bool, false> {};
 
-template <class... Tags> struct is_named_tuple<named_tuple<Tags...>> {
-  static constexpr bool const value = true;
-};
+template <class... Tags>
+struct is_named_tuple<named_tuple<Tags...>>
+    : std::integral_constant<bool, true> {};
 
 // is_unique_ptr
 
-template <class T> struct is_unique_ptr {
-  static constexpr bool const value = false;
+template <class T>
+struct is_unique_ptr : std::integral_constant<bool, false> {};
+
+template <class T>
+struct is_unique_ptr<std::unique_ptr<T>> : std::integral_constant<bool, true> {
 };
 
-template <class T> struct is_unique_ptr<std::unique_ptr<T>> {
-  static constexpr bool const value = true;
-};
+template <class Source, class Target>
+struct is_convertible
+    : std::integral_constant<bool,
+                             (std::is_arithmetic<Source>::value &&
+                              is_std_basic_string<Target>::value) ||
+                                 (std::is_arithmetic<Source>::value &&
+                                  std::is_arithmetic<Target>::value) ||
+                                 (is_std_basic_string<Source>::value &&
+                                  std::is_arithmetic<Target>::value) ||
+                                 (is_raw_string<Source>::value &&
+                                  std::is_arithmetic<Target>::value) ||
+                                 std::is_assignable<Target, Source>::value> {};
 
-template <class Source, class Target> struct is_convertible {
-  static constexpr bool const value =
-      (std::is_arithmetic<Source>::value &&
-       is_std_basic_string<Target>::value) ||
-      (std::is_arithmetic<Source>::value &&
-       std::is_arithmetic<Target>::value) ||
-      (is_std_basic_string<Source>::value &&
-       std::is_arithmetic<Target>::value) ||
-      (is_raw_string<Source>::value && std::is_arithmetic<Target>::value) ||
-      std::is_assignable<Target, Source>::value;
-};
-
-template <class Target, class Source> struct is_static_cast_assignable {
-  static constexpr bool const value =
-      std::is_arithmetic<Target>::value && std::is_arithmetic<Source>::value &&
-      !std::is_assignable<
-          Target,
-          Source>::value; //&& !std::is_convertible<Source,Target>::value;
-};
-
-template <class Source, class Tuple, size_t Index>
-struct tuple_member_assignable {
-  static constexpr bool const value =
-      std::is_assignable<std::tuple_element_t<Index, Tuple>, Source>::value;
-};
+template <class Target, class Source>
+struct is_static_cast_assignable
+    : std::integral_constant<
+          bool,
+          std::is_arithmetic<Target>::value &&
+              std::is_arithmetic<Source>::value &&
+              !std::is_assignable<Target,
+                                  Source>::
+                  value> //&& !std::is_convertible<Source,Target>::value;
+      {};
 
 template <class Source, class Tuple, size_t Index>
-struct tuple_member_convertible {
-  static constexpr bool const value =
-      std::is_convertible<Source, std::tuple_element_t<Index, Tuple>>::value &&
-      !std::is_assignable<std::tuple_element_t<Index, Tuple>, Source>::value;
-};
+struct tuple_member_assignable
+    : std::integral_constant<
+          bool,
+          std::is_assignable<std::tuple_element_t<Index, Tuple>,
+                             Source>::value> {};
 
 template <class Source, class Tuple, size_t Index>
-struct tuple_member_static_cast_assignable {
-  static constexpr bool const value =
-      is_static_cast_assignable<std::tuple_element_t<Index, Tuple>,
-                                Source>::value;
-};
+struct tuple_member_convertible
+    : std::integral_constant<
+          bool,
+          std::is_convertible<Source,
+                              std::tuple_element_t<Index, Tuple>>::value &&
+              !std::is_assignable<std::tuple_element_t<Index, Tuple>,
+                                  Source>::value> {};
 
 template <class Source, class Tuple, size_t Index>
-struct tuple_member_not_assignable {
-  static constexpr bool const value =
-      !tuple_member_assignable<Source, Tuple, Index>::value &&
-      !tuple_member_convertible<Source, Tuple, Index>::value &&
-      !tuple_member_static_cast_assignable<Source, Tuple, Index>::value;
-};
+struct tuple_member_static_cast_assignable
+    : std::integral_constant<
+          bool,
+          is_static_cast_assignable<std::tuple_element_t<Index, Tuple>,
+                                    Source>::value> {};
 
-template <class T> struct is_sub_object {
-  static constexpr bool const value =
-      is_named_tuple<T>::value || is_associative_container<T>::value;
-};
+template <class Source, class Tuple, size_t Index>
+struct tuple_member_not_assignable
+    : std::integral_constant<
+          bool,
+          !tuple_member_assignable<Source, Tuple, Index>::value &&
+              !tuple_member_convertible<Source, Tuple, Index>::value &&
+              !tuple_member_static_cast_assignable<Source, Tuple, Index>::
+                  value> {};
 
-template <class T> struct is_sub_element {
-  static constexpr bool const value = is_named_tuple<T>::value ||
-                                      is_sequence_container<T>::value ||
-                                      is_associative_container<T>::value;
-};
+template <class T>
+struct is_sub_object
+    : std::integral_constant<bool,
+                             is_named_tuple<T>::value ||
+                                 is_associative_container<T>::value> {};
+
+template <class T>
+struct is_sub_element
+    : std::integral_constant<bool,
+                             is_named_tuple<T>::value ||
+                                 is_sequence_container<T>::value ||
+                                 is_associative_container<T>::value> {};
 
 } // namespace named_types
